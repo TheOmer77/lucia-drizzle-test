8701273836019df14ec1b408bf32f452f65a0bdd
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-13 15:04:52 +0200

    Format with Prettier

--- src/components/ui/Card/Card.tsx
+++ src/components/ui/Card/Card.tsx
@@ -7,7 +7,7 @@ export const Card = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
     <div
       ref={ref}
       className={cn(
-        'bg-card text-card-foreground rounded-lg border shadow-sm',
+        'rounded-lg border bg-card text-card-foreground shadow-sm',
         className
       )}
       {...props}
