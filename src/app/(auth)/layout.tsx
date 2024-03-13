8602a33928ac12098b9f2e7199fe675034c29b33
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-14 00:22:17 +0200

    Prevent logged in users from accessing auth pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,18 @@
 import type { PropsWithChildren } from 'react';
+import { redirect } from 'next/navigation';
 
 import { Card } from '@/components/ui/Card';
+import { validateRequest } from '@/lib/auth';
 
-const AuthLayout = ({ children }: PropsWithChildren) => (
-  <div className='grid min-h-dvh w-full place-items-center p-4'>
-    <Card className='w-full max-w-md'>{children}</Card>
-  </div>
-);
+const AuthLayout = async ({ children }: PropsWithChildren) => {
+  const { user } = await validateRequest();
+  if (user) redirect('/');
+
+  return (
+    <div className='grid min-h-dvh w-full place-items-center p-4'>
+      <Card className='w-full max-w-md'>{children}</Card>
+    </div>
+  );
+};
 
 export default AuthLayout;
