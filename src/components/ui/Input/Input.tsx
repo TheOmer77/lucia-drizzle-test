2239f80e37b02a75b6cb7b0611ac2ddb484b3848
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-13 15:36:03 +0200

    Add Shadcn Input

---
+++ src/components/ui/Input/Input.tsx
@@ -0,0 +1,22 @@
+import { forwardRef, type ComponentPropsWithoutRef } from 'react';
+
+import { cn } from '@/lib/utils';
+
+export const Input = forwardRef<
+  HTMLInputElement,
+  ComponentPropsWithoutRef<'input'>
+>(({ className, ...props }, ref) => (
+  <input
+    {...props}
+    ref={ref}
+    className={cn(
+      `border-input bg-background ring-offset-background
+placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full
+rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent
+file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2
+focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50`,
+      className
+    )}
+  />
+));
+Input.displayName = 'Input';
