1d6e3bc5c9eef64168d43fb696ee12a9b0a8fe67
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-13 19:28:21 +0200

    Redirect to `/login` in `/` if logged out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
-const Home = () => <div>Hello world.</div>;
+import { redirect } from 'next/navigation';
+
+import { validateRequest } from '@/lib/auth';
+
+const Home = async () => {
+  const { user } = await validateRequest();
+  if (!user) redirect('/login');
+
+  return <div>Hello world.</div>;
+};
 
 export default Home;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,5 @@
+import { cache } from 'react';
+import { cookies } from 'next/headers';
 import { Lucia } from 'lucia';
 import { DrizzlePostgreSQLAdapter } from '@lucia-auth/adapter-drizzle';
 
@@ -13,6 +15,33 @@ export const lucia = new Lucia(adapter, {
     expires: false,
     attributes: { secure: process.env.NODE_ENV === 'production' },
   },
+});
+
+export const validateRequest = cache(async () => {
+  const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+  if (!sessionId) return { user: null, session: null };
+  const { user, session } = await lucia.validateSession(sessionId);
+  try {
+    if (session && session.fresh) {
+      const sessionCookie = lucia.createSessionCookie(session.id);
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      );
+    }
+    if (!session) {
+      const sessionCookie = lucia.createBlankSessionCookie();
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      );
+    }
+  } catch {
+    // Next.js throws error when attempting to set cookies when rendering page
+  }
+  return { user, session };
 });
 
 declare module 'lucia' {
