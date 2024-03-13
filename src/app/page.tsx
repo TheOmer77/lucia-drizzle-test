8c567df2354ba5607ce4e4ce2a86dbc8ba9313f1
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-14 00:05:04 +0200

    Make main page look somewhat better

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
 import { redirect } from 'next/navigation';
 
 import { validateRequest } from '@/lib/auth';
+import { Card } from '@/components/ui/Card';
 
 const Home = async () => {
   const { user } = await validateRequest();
   if (!user) redirect('/login');
 
-  return <div>Hello world.</div>;
+  return (
+    <div className='grid min-h-dvh w-full place-items-center p-4'>
+      <div>
+        <h1
+          className='mb-8 w-full text-center text-8xl font-extrabold
+tracking-tight'
+        >
+          Hello.
+        </h1>
+        <Card className='grid grid-cols-[auto,1fr] gap-4 p-4'>
+          <h2 className='col-span-full font-semibold'>Your user details:</h2>
+          {Object.entries(user).map(([key, value]) => (
+            <>
+              <span className='text-sm font-medium'>{key}:</span>
+              <span className='text-muted-foreground text-sm'>{value}</span>
+            </>
+          ))}
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
