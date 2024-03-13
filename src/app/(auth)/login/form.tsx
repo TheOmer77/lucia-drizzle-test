da85a61f5d563b2717b60b2fb955f8cada871081
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-13 19:32:56 +0200

    Add login page

---
+++ src/app/(auth)/login/form.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { CardContent } from '@/components/ui/Card';
+
+export const LoginForm = () => {
+  return <CardContent>Login form TBD.</CardContent>;
+};
---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+import { CardHeader, CardTitle } from '@/components/ui/Card';
+import { LoginForm } from './form';
+
+const LoginPage = () => (
+  <>
+    <CardHeader>
+      <CardTitle>Login</CardTitle>
+    </CardHeader>
+    <LoginForm />
+  </>
+);
+
+export default LoginPage;
