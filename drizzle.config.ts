52e2d842a7c644589e34fd33ae2ffcc86cb53c70
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-13 14:24:04 +0200

    Remove `verbose` and `strict` from Drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,8 +4,6 @@ const config = defineConfig({
   schema: './src/db/schema',
   driver: 'pg',
   dbCredentials: { connectionString: process.env.DB_URL! },
-  verbose: true,
-  strict: true,
 });
 
 export default config;
