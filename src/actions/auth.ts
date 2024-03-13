9c6e1d95406752935b1fdffa51816886ef7e9f60
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-13 18:07:45 +0200

    Add `registerUser` server action

---
+++ src/actions/auth.ts
@@ -0,0 +1,34 @@
+'use server';
+
+import { cookies } from 'next/headers';
+import { Argon2id } from 'oslo/password';
+
+import { signupFormSchema, type SignupFormValues } from '@/schemas/signup';
+import { db } from '@/db';
+import { user } from '@/db/schema';
+import { lucia } from '@/lib/auth';
+
+export const registerUser = async (values: SignupFormValues) => {
+  signupFormSchema.parse(values);
+  const { username, password } = values;
+
+  const hashedPassword = await new Argon2id().hash(password);
+
+  const [newUser] = await db
+    .insert(user)
+    .values({ username, password: hashedPassword })
+    .returning({ id: user.id, username: user.username });
+
+  const session = await lucia.createSession(newUser.id, {
+    expiresIn: 30 * 24 * 60 * 60,
+  });
+  const sessionCookie = lucia.createSessionCookie(session.id);
+
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+
+  return newUser;
+};
