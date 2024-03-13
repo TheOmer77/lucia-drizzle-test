3c119bba1f83bf4f648c4d97311db50b78f8423b
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-13 18:34:42 +0200

    Add Toaster to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import '@/styles/index.css';
+import { Toaster } from '@/components/ui/Toast';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,7 +14,10 @@ export const metadata: Metadata = {
 
 const RootLayout = ({ children }: PropsWithChildren) => (
   <html lang='en'>
-    <body className={inter.className}>{children}</body>
+    <body className={inter.className}>
+      {children}
+      <Toaster />
+    </body>
   </html>
 );
 
