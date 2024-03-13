944e2a7fcfb6e3018eb82067d4e64dcd3ac8fbeb
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-13 19:44:17 +0200

    Add login form & its schema

--- src/app/(auth)/login/form.tsx
+++ src/app/(auth)/login/form.tsx
@@ -1,7 +1,95 @@
 'use client';
 
-import { CardContent } from '@/components/ui/Card';
+import { useTransition } from 'react';
+import { redirect } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+import { Button } from '@/components/ui/Button';
+import { CardContent, CardFooter } from '@/components/ui/Card';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/Form';
+import { Input } from '@/components/ui/Input';
+import { useToast } from '@/hooks/useToast';
+import { loginFormSchema, type LoginFormValues } from '@/schemas/auth';
 
 export const LoginForm = () => {
-  return <CardContent>Login form TBD.</CardContent>;
+  const { displayToast } = useToast();
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<LoginFormValues>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: { username: '', password: '' },
+  });
+
+  const onSubmit = async (values: LoginFormValues) => {
+    startTransition(async () => {
+      const { success, data, error } = {
+        success: false,
+        error: new Error('Not implemented yet.'),
+        data: undefined,
+      };
+      if (!success) {
+        displayToast('Something went wrong', {
+          description:
+            error instanceof Error
+              ? error.message
+              : 'Failed to create a new user.',
+          variant: 'destructive',
+        });
+        return;
+      }
+
+      /* displayToast(`Welcome, ${data.username}!`, {
+        description: `You've successfully signed up.`,
+      });
+      redirect('/'); */
+    });
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <CardContent className='space-y-4'>
+          <FormField
+            control={form.control}
+            name='username'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='password'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input {...field} type='password' />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </CardContent>
+        <CardFooter className='justify-end'>
+          <Button type='submit' disabled={isPending}>
+            Login
+          </Button>
+        </CardFooter>
+      </form>
+    </Form>
+  );
 };
--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -1,5 +1,16 @@
 import { z } from 'zod';
 
+export const loginFormSchema = z.object({
+  username: z
+    .string()
+    .min(2, { message: 'Username must be at least 2 characters long.' })
+    .max(50, { message: "Username can't be longer than 50 characters." }),
+  password: z
+    .string()
+    .min(8, { message: 'Password must be at least 8 characters long.' }),
+});
+export type LoginFormValues = z.infer<typeof loginFormSchema>;
+
 export const signupFormSchema = z
   .object({
     username: z
