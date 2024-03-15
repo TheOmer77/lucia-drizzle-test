8508345828e3517c46e11b6802d0ed10bf8bd452
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-15 21:18:38 +0200

    Add `createdAt` and `updatedAt` to emailVerification

--- src/db/schema/emailVerification.ts
+++ src/db/schema/emailVerification.ts
@@ -10,6 +10,8 @@ export const emailVerification = pgTable('email_verification', {
     .notNull()
     .unique()
     .references(() => user.id),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
   expiresAt: timestamp('expires_at').notNull(),
 });
 
