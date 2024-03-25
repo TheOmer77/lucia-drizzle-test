dcc24522953350f74d8a196e8c439ccf89bf2a47
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-25 13:10:19 +0200

    Update email env vars to be more generic

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,16 +1,16 @@
 import { createTransport, type SendMailOptions } from 'nodemailer';
 
 const transporter = createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  auth: { user: process.env.GMAIL_USER, pass: process.env.GMAIL_PASSWORD },
+  host: process.env.MAIL_HOST,
+  port: Number(process.env.MAIL_PORT),
+  secure: process.env.MAIL_SECURE?.toLowerCase() === 'true',
+  auth: { user: process.env.MAIL_USER, pass: process.env.MAIL_PASSWORD },
 });
 
 export const sendEmail = async (
   options: Required<Pick<SendMailOptions, 'to' | 'subject' | 'html'>>
 ) =>
   await transporter.sendMail({
-    from: `"Omer's test app" <${process.env.GMAIL_USER}>`,
+    from: `"Omer's test app" <${process.env.MAIL_USER}>`,
     ...options,
   });
