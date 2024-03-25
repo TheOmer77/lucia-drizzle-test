6f4b40a410f918b94162fe39d971710fea8781ae
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-25 16:55:26 +0200

    Update theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,57 +1,80 @@
 import type { Config } from 'tailwindcss';
 
+const shades = [50, ...[...Array(9).keys()].map(key => (key + 1) * 100), 950];
+
 const config = {
-  darkMode: ['class'],
   content: ['./src/**/*.{ts,tsx}'],
   prefix: '',
   theme: {
     container: {
       center: true,
       padding: '2rem',
-      screens: {
-        '2xl': '1400px',
+      screens: { '2xl': '1400px' },
+    },
+    colors: {
+      inherit: 'inherit',
+      white: '#fff',
+      black: '#000',
+      transparent: 'transparent',
+
+      ...['primary', 'neutral', 'danger'].reduce(
+        (obj, colorName) => ({
+          ...obj,
+          [colorName]: [...shades].reduce(
+            (obj, shade) => ({
+              ...obj,
+              [shade]: `rgb(var(--color-${colorName}-${shade}) / <alpha-value>)`,
+            }),
+            {
+              DEFAULT: `rgb(var(--color-${colorName}-main))`,
+              active: `rgb(var(--color-${colorName}-active))`,
+              foreground: `rgb(var(--color-${colorName}-foreground))`,
+            }
+          ),
+        }),
+        {}
+      ),
+
+      border: 'rgb(var(--color-border))',
+      ring: 'rgb(var(--color-ring))',
+      background: 'rgb(var(--color-background))',
+      foreground: 'rgb(var(--color-foreground))',
+      secondary: {
+        DEFAULT: 'rgb(var(--color-secondary))',
+        foreground: 'rgb(var(--color-secondary-foreground))',
+      },
+      muted: {
+        DEFAULT: 'rgb(var(--color-muted))',
+        foreground: 'rgb(var(--color-muted-foreground))',
+      },
+      accent: {
+        DEFAULT: 'rgb(var(--color-accent))',
+        foreground: 'rgb(var(--color-accent-foreground))',
+      },
+      popover: {
+        DEFAULT: 'rgb(var(--color-popover))',
+        foreground: 'rgb(var(--color-popover-foreground))',
+      },
+      card: {
+        DEFAULT: 'rgb(var(--color-card))',
+        foreground: 'rgb(var(--color-card-foreground))',
+      },
+      input: {
+        DEFAULT: 'rgb(var(--color-input))',
+        hover: 'rgb(var(--color-input-hover))',
+        invalid: 'rgb(var(--color-input-invalid))',
       },
     },
     extend: {
-      colors: {
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+        'caret-blink': 'caret-blink 1.25s ease-out infinite',
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        lg: 'var(--border-radius)',
+        md: 'calc(var(--border-radius) - 2px)',
+        sm: 'calc(var(--border-radius) - 4px)',
       },
       fontFamily: { sans: ['var(--font-sans)', 'sans-serif'] },
       keyframes: {
@@ -67,11 +90,6 @@ const config = {
           '0%,70%,100%': { opacity: '1' },
           '20%,50%': { opacity: '0' },
         },
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-        'caret-blink': 'caret-blink 1.25s ease-out infinite',
       },
     },
   },
