d04ae880adc4bb1201a0513124557841930b9dbf
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-25 15:25:36 +0200

    Fix incorrect jiti import in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 import createJiti from 'jiti';
 const jiti = createJiti(new URL(import.meta.url).pathname);
 
-// Import env here to validate during build. Using jiti we can import .ts files :)
-jiti('./app/env');
+jiti('./src/config/env');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
