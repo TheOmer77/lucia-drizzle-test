2dbbf5afca11e30cb51c834d790263b95f0cd32f
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-25 15:07:00 +0200

    Setup T3 Env

---
+++ src/config/env.ts
@@ -0,0 +1,29 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const env = createEnv({
+  server: {
+    DB_URL: z.string().url(),
+    MAIL_HOST: z.string().url(),
+    MAIL_PASSWORD: z.string(),
+    MAIL_PORT: z
+      .string()
+      .transform(s => Number(s))
+      .pipe(z.number()),
+    MAIL_SECURE: z
+      .string()
+      .refine(s => s.toLowerCase() === 'true' || s.toLowerCase() === 'false')
+      .transform(s => s === 'true'),
+    MAIL_USER: z.string(),
+    NODE_ENV: z.enum(['development', 'production', 'test']),
+  },
+  runtimeEnv: {
+    DB_URL: process.env.DB_URL,
+    MAIL_HOST: process.env.MAIL_HOST,
+    MAIL_PASSWORD: process.env.MAIL_PASSWORD,
+    MAIL_PORT: process.env.MAIL_PORT,
+    MAIL_SECURE: process.env.MAIL_SECURE,
+    MAIL_USER: process.env.MAIL_USER,
+    NODE_ENV: process.env.NODE_ENV,
+  },
+});
